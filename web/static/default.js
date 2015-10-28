aa19e726d63cea41225a58027955d4d20aa9c8ae
RyuJaeMyung <posjmss@gmail.com>
2015-10-28 14:33:19 +0900

    기본 in/out(web), 모델 load, api 구현

---
+++ web/static/default.js
@@ -0,0 +1,34 @@
+function initButtonClick(){
+	$('button#submit').click(function(){
+		var json_data = {
+			pk : $('input[name="pk"]').val(),
+			author : $('input[name="author"]').val(),
+			title : $('input[name="title"]').val(),
+			forumid : $('input[name="forumid"]').val(),
+			text : $('input[name="text"]').val(),
+			url : $('input[name="url"]').val()
+		}
+
+		console.log(json_data);
+
+		$.ajax({
+			type:'POST',
+			url:'/query',
+			encoding:'utf-8',
+			contentType:'application/json; charset=UTF-8',
+			data: JSON.stringify(json_data),
+			dataType: "json",
+			success:function(dataFromServer){
+				console.log("success");
+				var is_troll = dataFromServer;
+				console.log(is_troll);
+			}
+		});
+		return false;
+	});
+}
+
+$(function(){
+	initButtonClick();
+});
+
---
+++ web/web_main.py
@@ -0,0 +1,39 @@
+import sys
+import json
+sys.path.append('../class/')
+
+import Ilwar
+from flask import render_template
+from flask import request
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+def load_model():
+	predict_model.load_model(save_path = "../predict_model/")
+
+def make_input(data):
+	return [data]
+
+@app.route('/')
+def home():
+	return render_template("index.html")
+
+@app.route('/query', methods = ['POST'])
+def test():
+	data = request.data.decode('utf-8')
+	input_data = make_input(json.loads(data))
+
+	result = predict_model.predict(input_data)
+	
+	response = {}
+	for i in range(0, len(input_data)):
+		response[str(input_data[i]["pk"])] = bool(result[i])
+
+	return jsonify(response)
+
+if __name__ == '__main__':
+	predict_model = Ilwar.TrollClassifier()
+	load_model()
+	app.run(debug=True)
