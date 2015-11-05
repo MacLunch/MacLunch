e8516af7a2f2b1974f726336eed4bc3fbc053a8e
RyuJaeMyung <posjmss@gmail.com>
2015-11-05 16:19:01 +0900

    navigation bar 수정/footer 추가/자잘한 수정

--- web/static/js/home.js
+++ web/static/js/home.js
@@ -1,16 +1,34 @@
+$(window).scroll(function(){
+	var scrollHeight = $(window).scrollTop();
+	var navBar = $("#full-navbar");
 
-function initialize() {
+	if(scrollHeight >= 700) {
+		if(navBar.hasClass("navbar-transparent") == true) {
+			navBar.removeClass("navbar-transparent");
+		}
+	} else {
+		if(navBar.hasClass("navbar-transparent") == false) {
+			navBar.addClass("navbar-transparent");
+		}
+	}
+});
+
+
+function init_map() {
     var mapCanvas = document.getElementById('map');
+
     var mapOptions = {
       center: new google.maps.LatLng(37.503656, 127.045008),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
+
     var map = new google.maps.Map(mapCanvas, mapOptions);
 }
+
 function eqfeed_callback(results) {
   map.data.addGeoJson(results);
 }
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', init_map);
